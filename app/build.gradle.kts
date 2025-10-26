203de3ce222e7c7addad3889482ef48548f528c6
Sergey842248 <fallahchristine09@gmail.com>
2025-10-26 20:00:15 +0100

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.future.aicenter"
         minSdk = 26
         targetSdk = 36
-        versionCode = 350
-        versionName = "3.5.0"
+        versionCode = 351
+        versionName = "3.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
